9a806e983225ddb1685eef27f3ef8335135612a6
Paul Seiffert <paul.seiffert@gmail.com>
2014-12-08 20:13:26 +0100

    Failing with a meaningful error msg

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+: ${FPM_PORT_9000_TCP_ADDR:?"Environment variable FPM_PORT_9000_TCP_ADDR missing."}
+: ${FPM_PORT_9000_TCP_PORT:?"Environment variable FPM_PORT_9000_TCP_PORT missing."}
+
 /bin/sed -i "s@<FPM_HOST>@${FPM_PORT_9000_TCP_ADDR}@" /etc/nginx/nginx.conf
 /bin/sed -i "s@<FPM_PORT>@${FPM_PORT_9000_TCP_PORT}@" /etc/nginx/nginx.conf
 
